93115bdbb8dfe661e347eb4ec306b1e56746062a
Christopher Lamlamay <Christopher@ChristophersMBP.lan>
2016-11-07 20:27:16 -0600

    forked repo

--- reps.rb
+++ reps.rb
@@ -3,3 +3,43 @@
 # `p _variablename_` prints the full Object
 # `puts _variablename_` prints the Object.to_s (.toString())
 # to run, just `ruby reps.rb`
+
+
+#Round 1
+
+words = ["hello", "what", "is", "up", "dude"]
+length = []
+words.each{ |a| length << a.length}
+length
+
+
+#Round 2
+# transmogrifier = (5 * 3)**2
+
+class Transmogrified
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def to_s
+    p (@a*@b)^@C
+  end
+end
+
+  @one = Transmogrified.new(5,4,3)
+  @two = Transmogrified.new(13,12,5)
+  @three = Transmogrified.new(42,13,7)
+
+
+
+#Round 3
+
+
+#Round 4
+
+#Round 5
+
+#Round 6
