8ebeef5435c4eb3262b3d67dad7abc8b4aea0daa
Christopher Lamlamay <Christopher@ChristophersMBP.lan>
2016-11-07 20:48:02 -0600

    finished the rounds, faster than expected! holy cow

--- reps.rb
+++ reps.rb
@@ -1,9 +1,3 @@
-# write your reps here
-# makes sure to either `p my_output` or `puts my_output`.
-# `p _variablename_` prints the full Object
-# `puts _variablename_` prints the Object.to_s (.toString())
-# to run, just `ruby reps.rb`
-
 
 #Round 1
 
@@ -33,13 +27,43 @@ end
   @two = Transmogrified.new(13,12,5)
   @three = Transmogrified.new(42,13,7)
 
-
-
 #Round 3
+
+
+class Toonify
+  def initialize(accent, sentence)
+    if accent == 'daffy'
+      p sentence.gsub('s', 'th')
+    elsif accent == 'elmer'
+      p sentence.gsub('r', 'w')
+    else
+      p sentence
+    end
+  end
+end
+
+@daffy = Toonify.new('daffy', 'You are dispiciable!')
+@elmer = Toonify.new('elmer', 'Be very, very quiet')
 
 
 #Round 4
 
+sentence = "Now I know what a TV dinner feels like"
+sentence.split(" ").reverse.join(" ")
+
 #Round 5
 
+letter = "Now I know what a TV dinner feels like"
+secondLetter = "Put Hans back on the line"
+
+letter.reverse
+secondLetter.reverse
+
+
 #Round 6
+
+long = ["oh", "good", "grief"]
+long.max_by(&:length)
+
+longest = ["Nothing" , "takes", "the", "taste", "out", "of", "peanut", "butter", "quite", "like", "unrequited", "love"]
+longest.max_by(&:length)
