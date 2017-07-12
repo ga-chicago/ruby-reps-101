133139b867b10c89783a9aad7cf2618b9f48c2b3
lindseygilmore <lindseygilmore21@gmail.com>
2017-07-11 20:50:47 -0500

    round 5 completed

--- reps.rb
+++ reps.rb
@@ -89,10 +89,24 @@ end
 
 word_reverse('I am having a great time')
 
-#ROUND 5
+#ROUND 5***
+# Write a function letterReverse that accepts a single argument, 
+# a string. The function should maintain the order of words in 
+# the string but reverse the letters in each word. 
+# Don't worry about punctuation. This will be very similar to 
+# round 4 except you won't need to split them with a space.
 
+# letterReverse("Now I know what a TV dinner feels like")
+# # => "woN I wonk tahw a VT rennid sleef ekil"
+# letterReverse("Put Hans back on the line")
+# # => "tuP snaH kcab no eht enil"
 
+def letter_reverse(num)
+	p num.reverse
+end
 
+letter_reverse('I am having a gret time')
+#=>
 
 
 
