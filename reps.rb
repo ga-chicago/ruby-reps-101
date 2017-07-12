e0d528f4ba25f4ee39e318d020d5e08bd6e01884
lindseygilmore <lindseygilmore21@gmail.com>
2017-07-11 20:33:43 -0500

    round 2 complete

--- reps.rb
+++ reps.rb
@@ -21,5 +21,44 @@ end
 leng_of_word(["the", "class", "was", "awesome"])
 
 # ROUND 2
+# Write a Ruby function called transmogrifier This function should 
+# accept three arguments, which you can assume will be numbers. 
+# Your function should return the "transmogrified" result
+
+# The transmogrified result of three numbers is the product 
+# (numbers multiplied together) of the first two numbers, 
+# raised to the power (exponentially) of the third number.
+
+# For example, the transmogrified result of 5, 3, and 2 is 
+# (5 times 3) to the power of 2 is 225.
+
+# Use your function to find the following answers.
+
+# transmogrifier(5, 4, 3)
+# transmogrifier(13, 12, 5)
+# transmogrifier(42, 13, 7)
+
+def transmogrifier(num1, num2, num3)
+	(num1 * num2) ** num3
+end
+
+p transmogrifier(5, 3, 2)
+p transmogrifier(5, 4, 3)
+p transmogrifier(13, 12, 5)
+p transmogrifier(42, 13, 7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
