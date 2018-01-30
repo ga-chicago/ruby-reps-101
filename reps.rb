51a65dc8c27661baf27f58456082f667256b3717
Anthony <anthonyjlower@gmail.com>
2018-01-29 18:51:08 -0600

    Completed lengths

--- reps.rb
+++ reps.rb
@@ -3,3 +3,15 @@
 # `p _variablename_` prints the full Object
 # `puts _variablename_` prints the Object.to_s (.toString())
 # to run, just `ruby reps.rb`
+
+# Round One
+# words = ["hello", "what", "is", "up", "dude"]
+
+# def lengths arr
+# 	lenArray = []
+# 	for	i in (0...arr.length)
+# 		lenArray << arr[i].length
+# 	end
+# 	p lenArray
+# end
+# lengths words
