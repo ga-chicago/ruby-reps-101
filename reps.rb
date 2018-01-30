c281085f53e2135c83c6b524229b046dbb06d24b
Michael Weber <webermn15@gmail.com>
2018-01-29 23:43:31 -0600

    Round 2

--- reps.rb
+++ reps.rb
@@ -4,9 +4,19 @@
 # `puts _variablename_` prints the Object.to_s (.toString())
 # to run, just `ruby reps.rb`
 
-# ROUND 1
-def lengths arr
-	arr.map { |i| i.length}
+# # ROUND 1
+# def lengths arr
+# 	arr.map { |i| i.length}
+# end
+
+# p lengths ["dog", "mouse", "cats"]
+
+
+# ROUND 2
+def transmogrifier numa, numb, numc
+	(numa * numb) ** numc
 end
 
-p lengths ["dog", "mouse", "cats"]
+p transmogrifier 5, 4, 3
+p transmogrifier 13, 12, 5
+p transmogrifier 42, 13, 7
