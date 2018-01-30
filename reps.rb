18a918a75710bd67cc8d0fb937fe19032421cb5f
Travis1282 <tmail20@gmail.com>
2018-01-29 19:46:35 -0600

    round one complete

--- reps.rb
+++ reps.rb
@@ -3,3 +3,16 @@
 # `p _variablename_` prints the full Object
 # `puts _variablename_` prints the Object.to_s (.toString())
 # to run, just `ruby reps.rb`
+
+
+# def length_five? input
+# 	str.length == 5 
+# end
+
+def lengths inputs 
+	p inputs.map { |input| input.length } 
+end 
+
+words = ["hello", "what", "is", "up", "dude"]
+lengths(words)  # => [5, 4, 2, 2, 4]
+
