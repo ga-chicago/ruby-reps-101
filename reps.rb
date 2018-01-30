b332c1d89fc79d3a0757e425e81e80b07310eca5
Sierra Moore <sierra.v.moore@gmail.com>
2018-01-29 20:12:39 -0600

    round 1 completed

--- reps.rb
+++ reps.rb
@@ -3,3 +3,18 @@
 # `p _variablename_` prints the full Object
 # `puts _variablename_` prints the Object.to_s (.toString())
 # to run, just `ruby reps.rb`
+
+
+# -------Round 1----------
+words = ["hello", "what", "is", "up", "dude"]
+
+def lengths stringsArr
+  stringsArr.map { |x| x.size }
+end
+p lengths(words)
+
+# ---------Round 2---------
+
+def transmogrifier num1, num2, num3
+
+end
