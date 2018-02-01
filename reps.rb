d543fba1c191692946bab6506734a43bc751e340
sergio <scrummish@gmail.com>
2018-01-31 20:51:47 -0600

    First commit, did the first two and forgot i had to commit

--- reps.rb
+++ reps.rb
@@ -3,3 +3,24 @@
 # `p _variablename_` prints the full Object
 # `puts _variablename_` prints the Object.to_s (.toString())
 # to run, just `ruby reps.rb`
+
+# Round 1
+# def lengths strArray
+# 	strArray.map{ |i| i.length }
+# end
+
+# p lengths ["hello", "what", "is", "up", "dude"]
+
+# Round 2
+# def transmogrifier num1, num2, num3
+# 	(num1*num2)**num3
+# end
+
+# p transmogrifier 5, 4, 3
+# p transmogrifier 13, 12, 5
+# p transmogrifier 42, 13, 7
+
+# Round 3
+
+
+
