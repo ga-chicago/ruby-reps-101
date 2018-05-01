582abfe258e37bf2890bab809fbd94097ef460ba
Tyler Earls <tyler.a.earls@gmail.com>
2018-05-01 13:25:14 -0500

    finished round 2

--- reps.rb
+++ reps.rb
@@ -5,9 +5,20 @@
 # to run, just `ruby reps.rb`
 
 # Round 1
+
 words = ["hello", "what", "is", "up", "dude"]
 def lengths arr_strings
 	arr_strings.map!{|str| str = str.length}
 end
 
 p lengths words
+
+# Round 2 
+
+def transmogrifier x, y, z
+	(x * y) **z
+end
+p transmogrifier 5, 4, 3
+p transmogrifier 13, 12, 5
+p transmogrifier 42, 13, 7
+
