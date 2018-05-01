b195eb83f22a8838ee144b206d6fdd0d4de24136
Hannah Werman <hannah.werman@gmail.com>
2018-05-01 17:14:57 -0500

    round 1 complete

--- reps.rb
+++ reps.rb
@@ -3,3 +3,11 @@
 # `p _variablename_` prints the full Object
 # `puts _variablename_` prints the Object.to_s (.toString())
 # to run, just `ruby reps.rb`
+
+def lengths arr
+	arr_of_lengths = []
+	arr.each { |i| arr_of_lengths.push(i.length)}
+	return arr_of_lengths
+end
+
+p lengths ['I', 'want', 'some', 'pizza', 'immediately']
