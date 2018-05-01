c23875a9a44679e1e762315e9870c814accaf23c
Tyler Earls <tyler.a.earls@gmail.com>
2018-05-01 12:46:16 -0500

    finished Round 1

--- reps.rb
+++ reps.rb
@@ -3,3 +3,11 @@
 # `p _variablename_` prints the full Object
 # `puts _variablename_` prints the Object.to_s (.toString())
 # to run, just `ruby reps.rb`
+
+# Round 1
+words = ["hello", "what", "is", "up", "dude"]
+def lengths arr_strings
+	arr_strings.map!{|str| str = str.length}
+end
+
+p lengths words
