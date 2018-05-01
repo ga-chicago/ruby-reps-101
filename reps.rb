77a43cfe426ec133af1d76c22cd86235eed158fe
marietrull <marietrull@gmail.com>
2018-05-01 13:21:49 -0500

    Completed Round 3

--- reps.rb
+++ reps.rb
@@ -22,3 +22,45 @@ end
 transmogrifier 5,3,2
 transmogrifier 13, 12, 5
 transmogrifier 42, 13, 7
+
+# -------------ROUND THREE-------------#
+
+def toonify accent, sentence
+	if accent == "daffy"
+		p sentence.gsub("s","th")
+	elsif accent == "elmur"
+		p sentence.gsub("r","w")
+	else 
+		p sentence
+	end
+end
+
+toonify "daffy", "so you smell like sausage"
+toonify "elmur", "what a waste of water"
+toonify "bugs", "i like carrots"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
